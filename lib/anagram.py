7420096beb4c687704c9827cf51a056f6c7ac7e8
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-26 12:15:13 +0300

    Update anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        return [w for w in words if self.is_anagram(w)]
+
+    def is_anagram(self, other_word):
+        return sorted(self.word.lower()) == sorted(other_word.lower())
